4aa220ed73e6db78da8ad3e6babbbd1e536fa89d
Dream Engine <dustinboveemusic@gmail.com>
2026-01-19 22:52:38 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const hero = document.querySelector('.hero');
-  if (hero.classList.contains('reveal')) {
-    setTimeout(function() {
-      hero.classList.add('show');
-    }, 1000);
+  const waitlistForm = document.getElementById("waitlist-form");
+  if (waitlistForm) {
+    waitlistForm.addEventListener("submit", function(event) {
+      event.preventDefault();
+      // Add logic for submitting form
+    });
   }
 });
